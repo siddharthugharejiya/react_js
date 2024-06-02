696382c3b8cf3d91e88d4ff3e1a20188f91107d6
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-06-03 00:02:41 +0530

    day-2

---
+++ day-2/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+import Portfolio from './Portfolio';
+import VotingApp from './VotingApp';
+
+function App() {
+  return (
+      <div>  
+        <VotingApp/>
+ 
+         <Portfolio/>
+         <Counter/>
+        </div>
+
+  );
+}
+
+export default App;
