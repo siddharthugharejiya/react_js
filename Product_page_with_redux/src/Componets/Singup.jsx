2a061496638301e6649836bb7a01d29a2d2ecf8a
siddharthugharejiya <multiera95@gmail.com>
2024-08-05 17:37:03 +0530

    .

--- Product_page_with_redux/src/Componets/Singup.jsx
+++ Product_page_with_redux/src/Componets/Singup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { SignupAction } from '../Redux/action';
 import { useNavigate } from 'react-router-dom';
+import "./Singup.css"
 
 
 const Singup = () => {
@@ -29,30 +30,63 @@ const Singup = () => {
   };
 
   return (
-    <form onSubmit={Submit}>
+    <>
+    {/* <form onSubmit={Submit}>
       <input
         type="text"
         name="username"
         value={formState.username}
         onChange={handleChange}
         placeholder="Username"
-      />
+        />
       <input
         type="email"
         name="email"
         value={formState.email}
         onChange={handleChange}
         placeholder="Email"
-      />
+        />
       <input
         type="password"
         name="pass"
         value={formState.pass}
         onChange={handleChange}
         placeholder="Password"
-      />
+        />
       <button type="submit" className='btn btn-dark'>Signup</button>
+    </form> */}
+
+<div className="main">
+
+
+<section class="background-texture"></section>
+  <div class="card-container">
+    <h2>Create Account</h2>
+    <form class="form" onSubmit={Submit}>
+      <input type="text"
+        name="username"
+        value={formState.username}
+        onChange={handleChange}
+        placeholder="Username" />
+      <input  type="email"
+        name="email"
+        value={formState.email}
+        onChange={handleChange}
+        placeholder="Email" />
+      <input  type="password"
+        name="pass"
+        value={formState.pass}
+        onChange={handleChange}
+        placeholder="Password"/>
+      <button type="submit">SIGN UP</button>
     </form>
+    <footer>
+      Already have an account?
+      <a href="#">Sign In here</a>
+    </footer>
+  </div>
+  </div>
+        </>
   );
 };
 
