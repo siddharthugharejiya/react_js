ed8e849a17dc65fd01ddada7d12673ad3fa681e7
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-08-07 11:46:33 +0530

    .

--- firebase/src/Componets/Firebase_Componets.jsx
+++ firebase/src/Componets/Firebase_Componets.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import { addDoc , collection, getDocs } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../Firebase/Firebase'
+
 
 const Firebase_Componets = () => {
+     const [state,setstate]=useState({
+      email:"",
+      password:""
+     })
+const [arr,setarr]=useState([])
+     const change = (e) =>{
+      const {name,value}=e.target
+        setstate({
+          ...state,
+          [name]:value
+
+        })
+     }
+
+     const submit = async(e) =>{
+      e.preventDefault()
+      let obj={
+        email:state.email,
+        password:state.password
+      }
+
+   let data=await addDoc(usercollection,obj)
+   console.log(data);
+   alert("data add successfully")
+    }
+     const usercollection = collection(db,"user")
+
+   const get=async()=>{
+     const getted=await getDocs(usercollection)
+     console.log(getted);
+     let d=getted.docs.map((el)=>{
+      return{
+        id:el.id,
+        ...el.data()
+      }
+    })
+    setarr(d)
+   }
+   console.log(arr);
+   
+   useEffect(()=>{
+    get()
+   },[])
   return (
     <div>
-     <form action="">
-        <input type="text" />
-        <input type="text" />
+     <form action="" onSubmit={submit}>
+        <input type="text" name='email' value={state.email} onChange={change} />
+        <input type="text" value={state.password} name='password' onChange={change} />
         <input type="submit" />
      </form>
+     {
+      arr.map((el)=>{
+        return(
+         <>
+         <p>{el.id}</p>
+          <p>{el.email}</p>
+          <p>{el.password}</p>
+         </> 
+        )
+      })
+     }
     </div>
   )
 }
