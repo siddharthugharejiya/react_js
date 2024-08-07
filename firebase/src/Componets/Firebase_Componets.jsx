db50efc16b2139806175509ce7a7322a0337815d
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-08-07 12:35:15 +0530

    .

--- firebase/src/Componets/Firebase_Componets.jsx
+++ firebase/src/Componets/Firebase_Componets.jsx
@@ -1,72 +1,82 @@
-import { addDoc , collection, getDocs } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { db } from '../Firebase/Firebase'
 
+const Firebase_Components = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  });
+  const [arr, setArr] = useState([]);
+  const usercollection = collection(db, "user");
 
-const Firebase_Componets = () => {
-     const [state,setstate]=useState({
-      email:"",
-      password:""
-     })
-const [arr,setarr]=useState([])
-     const change = (e) =>{
-      const {name,value}=e.target
-        setstate({
-          ...state,
-          [name]:value
+  const change = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value
+    });
+  };
 
-        })
-     }
+  const submit = async (e) => {
+    e.preventDefault();
+    let obj = {
+      email: state.email,
+      password: state.password
+    };
 
-     const submit = async(e) =>{
-      e.preventDefault()
-      let obj={
-        email:state.email,
-        password:state.password
-      }
+    await addDoc(usercollection, obj);
+    alert("Data added successfully");
+    get(); 
+    setState({ email: "", password: "" }); 
+  };
 
-   let data=await addDoc(usercollection,obj)
-   console.log(data);
-   alert("data add successfully")
-    }
-     const usercollection = collection(db,"user")
-
-   const get=async()=>{
-     const getted=await getDocs(usercollection)
-     console.log(getted);
-     let d=getted.docs.map((el)=>{
-      return{
-        id:el.id,
+  const get = async () => {
+    const getted = await getDocs(usercollection);
+    let d = getted.docs.map((el) => {
+      return {
+        id: el.id,
         ...el.data()
-      }
-    })
-    setarr(d)
-   }
-   console.log(arr);
-   
-   useEffect(()=>{
-    get()
-   },[])
+      };
+    });
+    setArr(d);
+  };
+
+  useEffect(() => {
+    get();
+  }, []); 
+
+  const del = async (id) => {
+    let d = doc(db, "user", id);
+    await deleteDoc(d);
+    get(); 
+    alert("Deleted successfully");
+  };
+
   return (
     <div>
-     <form action="" onSubmit={submit}>
-        <input type="text" name='email' value={state.email} onChange={change} />
-        <input type="text" value={state.password} name='password' onChange={change} />
-        <input type="submit" />
-     </form>
-     {
-      arr.map((el)=>{
-        return(
-         <>
-         <p>{el.id}</p>
-          <p>{el.email}</p>
-          <p>{el.password}</p>
-         </> 
-        )
-      })
-     }
+      <form onSubmit={submit}>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={state.email} onChange={change} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="text" name="password" value={state.password} onChange={change} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {arr.map((el) => (
+          <div key={el.id}>
+            <div>{el.email}</div>
+            <div>{el.password}</div>
+            <button onClick={() => del(el.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Firebase_Componets
+export default Firebase_Components;
