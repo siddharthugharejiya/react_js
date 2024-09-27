068f344b9db7f770e8779954bd12c13c7ddda364
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-09-27 12:08:29 +0530

    navbar

--- website/src/Compontes/Navbar_1.jsx
+++ website/src/Compontes/Navbar_1.jsx
@@ -1,111 +1,157 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
+import { Link } from "react-router-dom";
 
 function Navbar_1() {
+  const [activeCategory, setActiveCategory] = useState("Dairy & Bakery");
+
+  const categories = {
+    "Dairy & Bakery": {
+      Dairy: ["Milk", "Ice Cream", "Cheese", "Frozen Custard", "Frozen Yogurt"],
+      Bakery: [
+        "Cake And Pastry",
+        "Rusk Toast",
+        "Bread & Buns",
+        "Chocolate Brownie",
+        "Cream Roll",
+      ],
+    },
+    "Fruits & Vegetables": {
+      Fruits: ["Apples", "Bananas", "Oranges"],
+      Vegetables: ["Carrots", "Broccoli", "Spinach"],
+    },
+    "Snack & Spice": {
+      Snacks: ["Chips", "Popcorn", "Nuts"],
+      Spice: ["Cumin", "Coriander", "Turmeric"],
+    },
+    "Juice & Drinks": {
+      Juice: ["Orange Juice", "Apple Juice", "Grape Juice"],
+      Drinks: ["Soda", "Water", "Tea"],
+    },
+  };
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary flex-wrap">
-      <Container id="con">
-        <Navbar.Brand to={"/"}>
-          <img
-            src="./public/image/logo.png"
-            class="img-fluid rounded-top"
-            alt=""
-          />
-        </Navbar.Brand>
+    <div className="container">
+      <Navbar expand="lg" className="flex-wrap">
+        <Container id="con">
+          <Navbar.Brand as={Link} to={"/"}>
+            <img
+              src="./public/image/logo.png"
+              className="img-fluid rounded-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
 
-        <div className="col-xl-5 col-lg-5 col-md-8 col-sm-8 d-flex in">
-          <input type="search" placeholder="Search for items..." id="search" />
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              All Catogaries
-            </Dropdown.Toggle>
+          <div className="col-12 col-lg-5 d-flex align-items-center">
+            <input
+              type="search"
+              placeholder="Search for items..."
+              id="search"
+              // className="form-control"
+            />
+            <Dropdown>
+              <Dropdown.Toggle id="dropdown-basic" style={{ background: "transparent", color: "black" }}>
+                All Categories
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Men</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Woman</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Electronics</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <div className="col-1 glass fs-light">
-            <i
-              class="fa-solid fa-magnifying-glass"
-              style={{ color: "#ffffff" }}
-            ></i>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Men</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Women</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Electronics</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <div className="col-1 glass fs-light">
+              <i
+                className="fa-solid fa-magnifying-glass"
+                style={{ color: "#ffffff" }}
+              ></i>
+            </div>
           </div>
-          <div>
-        </div>
-        </div>
-        <div className="col-3 d-flex justify-content-around">
-        <div className="row">
-          <div className="col-xl-12 d-flex " style={{ height: "25px" }}>
-          <i class="ri-user-line"
-              style={{fontWeight:400,  margin:"0px 5px" ,fontSize:"20px"}}
-            ></i>
 
-            <div className="col-xl-9">
-              <div class="paste-button">
-                <button className="button" style={{fontWeight:500}}>Account</button>
-                <div class="dropdown-content">
-                  <Link id="top" to={"/signup"}>
-                    Register
-                  </Link>
-                  <Link id="middle" to={"/checkout"}>
-                    Checkout
-                  </Link>
-                  <Link id="bottom" to={"/login"}>
-                    Login
-                  </Link>
+          <div className="col-12 col-lg-3 d-flex justify-content-around align-items-center">
+            <div className="d-flex align-items-center">
+              <i className="ri-user-line" style={{ fontSize: "20px", margin: "0px 5px" }}></i>
+              <div>
+                <div className="paste-button">
+                  <button className="button" style={{ fontWeight: 500 }}>Account</button>
+                  <div className="dropdown-content">
+                    <Link id="top" to={"/signup"}>Register</Link>
+                    <Link id="middle" to={"/checkout"}>Checkout</Link>
+                    <Link id="bottom" to={"/login"}>Login</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex align-items-center">
+              <i className="ri-heart-line" style={{ color: "black", margin: "0px 5px", fontSize: "20px" }}></i>
+              <div>Wishlist</div>
+            </div>
+
+            <div className="d-flex align-items-center">
+              <i className="ri-shopping-cart-line" style={{ color: "black", margin: "0px 5px", fontSize: "20px" }}></i>
+              <div>Cart</div>
+            </div>
+          </div>
+
+          <div className="col-12 col-lg-4">
+            <div className="paste-button">
+              <button className="button" style={{ fontWeight: 500 }}>
+                Category Menu
+              </button>
+              <div className="dropdown-content">
+                <div className="container">
+                  <div className="navbar-section">
+                    <div className="tabs">
+                      {Object.keys(categories).map((category) => (
+                        <button
+                          key={category}
+                          className={`tab-btn ${activeCategory === category ? "active" : ""}`}
+                          onClick={() => handleCategoryClick(category)}
+                        >
+                          {category}
+                        </button>
+                      ))}
+                    </div>
+                    <div className="tab-content">
+                      {Object.keys(categories[activeCategory]).map((subCategory) => (
+                        <div key={subCategory} id="tab-c">
+                          <h4>{subCategory}</h4>
+                          <ul>
+                            {categories[activeCategory][subCategory].map((item) => (
+                              <li key={item}>{item}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="row">
-          <div className="col-xl-12 d-flex " style={{ height: "25px" }}>
-            <div>
-            <i class="ri-heart-line"
-                style={{ color: "black",margin:"0px 5px" ,fontSize:"20px"}}
-              ></i>
-            </div>
-            <div className="col-xl-9">Wishlist</div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-xl-12 d-flex " style={{ height: "25px" }}>
-            <div>
-            <i class="ri-shopping-cart-line"
-                style={{ color: "black",margin:"0px 5px" ,fontSize:"20px"}}
-              ></i>
-            </div>
-            <div className="col-xl-9">cart</div>
-          </div>
-        </div>
-        </div>
-      
-        <div className="row">
           <div className="col-12">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/product"}>Product</Link>
-            <Link to={"/signup"}>Signup</Link>
-            <Link to={"/login"}>Login</Link>
-          </Nav>
-        </Navbar.Collapse>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to={"/"}>Home</Link>
+                <Link to={"/product"}>Product</Link>
+                <Link to={"/signup"}>Signup</Link>
+                <Link to={"/login"}>Login</Link>
+              </Nav>
+            </Navbar.Collapse>
           </div>
-        </div>
-       
-      </Container>
-      
-    </Navbar>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
