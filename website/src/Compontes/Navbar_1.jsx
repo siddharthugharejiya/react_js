1f2cc5bb608fa34af140e88bf578082ef68a7110
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-09-28 21:20:08 +0530

    navbar

--- website/src/Compontes/Navbar_1.jsx
+++ website/src/Compontes/Navbar_1.jsx
@@ -69,7 +69,7 @@ function Navbar_1() {
                 <Dropdown.Item href="#/action-3">Electronics</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-            <div className="col-1 glass fs-light">
+            <div className="col-2 col-sm-2 glass fs-light">
               <i
                 className="fa-solid fa-magnifying-glass"
                 style={{ color: "#ffffff" }}
@@ -77,7 +77,7 @@ function Navbar_1() {
             </div>
           </div>
 
-          <div className="col-12 col-lg-3 d-flex justify-content-around align-items-center">
+          <div className="col-12 col-lg-3 d-flex justify-content-around align-items-center" >
             <div className="d-flex align-items-center">
               <i
                 className="ri-user-line"
@@ -122,205 +122,178 @@ function Navbar_1() {
           <div className="bor"></div>
         </div>
 
-        <Container>
-          <div className="col-12 col-lg-4">
-            <div className="paste-button">
-              <button className="button" style={{ fontWeight: 500 }}>
-                <i
-                  class="ri-bar-chart-horizontal-line border p-2"
-                  style={{ borderRadius: "4px" }}
-                ></i>
-              </button>
-              <div className="dropdown-content">
-                <div className="container">
-                  <div className="navbar-section">
-                    <div className="tabs">
-                      {Object.keys(categories).map((category) => (
-                        <button
-                          key={category}
-                          className={`tab-btn ${
-                            activeCategory === category ? "active" : ""
-                          }`}
-                          onClick={() => handleCategoryClick(category)}
-                        >
-                          {category}
-                        </button>
-                      ))}
-                    </div>
-                    <div className="tab-content">
-                      {Object.keys(categories[activeCategory]).map(
-                        (subCategory) => (
-                          <div key={subCategory} id="tab-c">
-                            <h4>{subCategory}</h4>
-                            <ul>
-                              {categories[activeCategory][subCategory].map(
-                                (item) => (
-                                  <li key={item}>{item}</li>
-                                )
-                              )}
-                            </ul>
-                          </div>
-                        )
-                      )}
-                    </div>
-                  </div>
+        <Container className="d-flex flex-wrap justify-content-between align-items-center bb" >
+      <div className="col-3 col-lg-1 d-flex ">
+        <div className="paste-button">
+          <button className="button" style={{ fontWeight: 500 }}>
+            <i
+              className="ri-bar-chart-horizontal-line border p-2"
+              style={{ borderRadius: "4px" }}
+            ></i>
+          </button>
+          <div className="dropdown-content">
+            <div className="container">
+              <div className="navbar-section">
+                <div className="tabs">
+                  {Object.keys(categories).map((category) => (
+                    <button
+                      key={category}
+                      className={`tab-btn ${
+                        activeCategory === category ? "active" : ""
+                      }`}
+                      onClick={() => handleCategoryClick(category)}
+                    >
+                      {category}
+                    </button>
+                  ))}
+                </div>
+                <div className="tab-content">
+                  {Object.keys(categories[activeCategory]).map(
+                    (subCategory) => (
+                      <div key={subCategory} id="tab-c">
+                        <h4>{subCategory}</h4>
+                        <ul>
+                          {categories[activeCategory][subCategory].map(
+                            (item) => (
+                              <li key={item}>{item}</li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    )
+                  )}
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <div className="col-12">
-            {/* <div className="d-flex align-items-center">
-              <i className="ri-user-line" style={{ fontSize: "20px", margin: "0px 5px" }}></i>
-              <div>
-                <div className="paste-button nav2">
-                  <button className="button" style={{ fontWeight: 500 }}>Account</button>
-                  <ul>
-                  <div className="dropdown-content">
-                    <Link id="top" to={"/signup"}>Register</Link>
-                    <Link id="middle" to={"/checkout"}>Checkout</Link>
-                    <Link id="bottom" to={"/login"}>Login</Link>
-                  </div>
-                  </ul>
-                  
+ 
+      <div className="col-9 col-lg-8 col-xl-7 drop">
+        <nav className="nav2 d-flex justify-content-center">
+          <ul className="d-flex justify-content-between">
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Home <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                  Grocery
+                  </a>
+                  <a id="middle" href="#">
+                   Fashion
+                  </a>
+                  {/* <a id="bottom" href="#">
+                    Keep text only
+                  </a> */}
                 </div>
               </div>
-            </div> */}
-            <nav class="nav2">
-              <ul>
-                <li>
-                  <div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Home <i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Catogory <i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  </li>
-                <li>
-                <div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Products<i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li><div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Pages <i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  </li>
-                <li>
-                <div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Blog <i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
+            </li>
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Category <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                  Shop Left sidebar
+                  </a>
+                  <a id="middle" href="#">
+                  Shop Right sidebar
+                  </a>
+                  <a id="bottom" href="#">
+                  Full Width
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Products <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                  product Left sidebar 
+                  </a>
+                  <a id="middle" href="#">
+                  product Right sidebar 
+                  </a>
+                  <a id="bottom" href="#">
+                  Product Full Width 
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Pages <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                  About Us
+                  </a>
+                  <a id="middle" href="#">
+                  Contact Us
+                  </a>
+                  <a id="bottom" href="#">
+                  Cart
+                  </a>
+                  <a id="bottom" href="#">
+                  Checkout
+                  </a>
+             
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Blog <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                    Keep source formatting
+                  </a>
+                  <a id="middle" href="#">
+                    Merge formatting
+                  </a>
+                  <a id="bottom" href="#">
+                    Keep text only
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="paste-button">
+                <button className="button d-flex">
+                  Elements <i className="ri-arrow-down-s-line arrow-icon"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a id="top" href="#">
+                    Keep source formatting
+                  </a>
+                  <a id="middle" href="#">
+                    Merge formatting
+                  </a>
+                  <a id="bottom" href="#">
+                    Keep text only
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-                <li>
-                <div>
-                    <div class="paste-button">
-                      <button className="button d-flex">
-                        Elements <i class="ri-arrow-down-s-line arrow-icon "></i>
-                      </button>
-                      <div class="dropdown-content">
-                        <a id="top" href="#">
-                          Keep source formatting
-                        </a>
-                        <a id="middle" href="#">
-                          Merge formatting
-                        </a>
-                        <a id="bottom" href="#">
-                          Keep text only
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </nav>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Link to={"/"}>Home</Link>
-                <Link to={"/product"}>Product</Link>
-                <Link to={"/signup"}>Signup</Link>
-                <Link to={"/login"}>Login</Link>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        </Container>
+  
+      <div className="col-xl-2 col-lg-4 col-md-4 d-flex justify-content-center align-items-center" id="phone">
+        <i className="ri-phone-line"></i> +123 (456) (7890)
+      </div>
+    </Container>
       </Navbar>
     </div>
   );
